58752ef0ab8c3fcacdf5f47711f5ad1b5d1f45f7
Bhalsod Nikunj Hiteshbhai <316708971+bhalsodnikunjhiteshbhai@users.noreply.github.com>
2026-08-19 02:57:02 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const authRoutes = require('./routes/auth');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect('mongodb://127.0.0.1:27017/notes_app')
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.error(err));
+
+app.use('/auth', authRoutes);
+
+app.listen(5000, () => console.log('Server started on port 5000'));
