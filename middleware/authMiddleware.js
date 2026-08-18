a3bacff4f27cb38980c87ec85c92633072bbece3
Bhalsod Nikunj Hiteshbhai <316708971+bhalsodnikunjhiteshbhai@users.noreply.github.com>
2026-08-19 02:59:33 +0530

    Add files via upload

---
+++ middleware/authMiddleware.js
@@ -0,0 +1,15 @@
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+
+module.exports = function (req, res, next) {
+    const token = req.headers['authorization'];
+    if (!token) return res.status(401).json({ message: 'No token provided' });
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        req.user = decoded;
+        next();
+    } catch {
+        res.status(403).json({ message: 'Invalid token' });
+    }
+};
